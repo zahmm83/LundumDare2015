5d41203e6097656a2f9124e869cc19549e9ad164
zahmm83 <zahmm83@uwosh.edu>
2015-08-22 15:44:39 -0500

    added health display and a start to player death/networking

--- Assets/Scripts/StatsController.cs
+++ Assets/Scripts/StatsController.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class StatsController : MonoBehaviour {
+public class StatsController : NetworkBehaviour {
 
+    [SyncVar]
     public int health = 500;
+    Text healthText;
 
 	// Use this for initialization
 	void Start () {
-	
+        this.healthText = GameObject.Find("Health Text").GetComponent<Text>();
+        SetHealthText();
 	}
 	
 	// Update is called once per frame
@@ -15,19 +20,34 @@ public class StatsController : MonoBehaviour {
 	
 	}
 
+    void SetHealthText()
+    {
+        if (isLocalPlayer)
+        {
+            this.healthText.text = "Health: " + this.health.ToString();
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         this.health -= damage;
+        SetHealthText();
 
-        if(this.health <= 0)
+        if (this.health <= 0)
         {
-            PlayerDied();
+            CmdTellServerYouDied(this.gameObject);
         }
     }
 
     public void PlayerDied()
     {
         Debug.Log("Hey now, you're supposed to be dead... stop running around.");
+    }
+
+    [Command]
+    void CmdTellServerYouDied(GameObject player)
+    {
+        
     }
 
 }
