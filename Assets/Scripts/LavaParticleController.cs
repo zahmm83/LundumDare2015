a2538a9a73d879ada3ce9e39666ec38d882c6ec8
Michael <lenzm00@uwosh.edu>
2015-08-24 15:16:14 -0500

    Lava particles mostly work (small issues with player death)

--- Assets/Scripts/LavaParticleController.cs
+++ Assets/Scripts/LavaParticleController.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class LavaParticleController : MonoBehaviour
 {
     public GameObject lavaParticles;
+    public GameObject deathParticles;
     private Vector3 offset = new Vector3(0, -1, 0);
 
     void OnCollisionEnter(Collision lava)
     {
-        if (lava.gameObject.tag.Equals("Lava"))
+        if (GetComponent<StatsController>().isNotDead && lava.gameObject.tag.Equals("Lava"))
         {
             GameObject particles = Instantiate(lavaParticles, transform.position - offset, Quaternion.identity) as GameObject;
             Destroy(particles, 3);
