503bf81477813fcdea5523d714fc858197c265f6
Michael <lenzm00@uwosh.edu>
2015-08-23 23:08:19 -0500

    Menu fully works

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void OnLevelWasLoaded(int level)
+    {
+        //Menu has to always have the index 0 in build settings
+        if (level == 0)
+            SetupMenuButtons();
+    }
+
+    public void SetupMenuButtons()
+    {
+        GameNetworkManager networkManager = GameObject.Find("NetworkManager").GetComponent<GameNetworkManager>();
+
+        GameObject.Find("ButtonSingleplayer").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonSingleplayer").GetComponent<Button>().onClick.AddListener(networkManager.StartSingleplayer);
+
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(networkManager.StartupHost);
+
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonJoinGame").GetComponent<Button>().onClick.AddListener(networkManager.JoinGame);
+
+        GameObject.Find("ButtonPlayerName").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonPlayerName").GetComponent<Button>().onClick.AddListener(networkManager.SetPlayerName);
+
+        Button triceratot = GameObject.Find("Triceratot").GetComponent<Button>();
+        triceratot.onClick.RemoveAllListeners();
+        triceratot.onClick.AddListener(() => networkManager.SetCharacterSelection(triceratot));
+
+        Button squareBeast = GameObject.Find("Squarebeast").GetComponent<Button>();
+        squareBeast.onClick.RemoveAllListeners();
+        squareBeast.onClick.AddListener(() => networkManager.SetCharacterSelection(squareBeast));
+    }
+}
