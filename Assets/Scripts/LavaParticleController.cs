2894f5aa8bb6e3d210567b8e5dafa430d0c99f1f
Michael <lenzm00@uwosh.edu>
2015-08-24 14:30:03 -0500

    Added lava splash effect

---
+++ Assets/Scripts/LavaParticleController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaParticleController : MonoBehaviour
+{
+    public GameObject lavaParticles;
+    private Vector3 offset = new Vector3(0, -1, 0);
+
+    void OnCollisionEnter(Collision lava)
+    {
+        if (lava.gameObject.tag.Equals("Lava"))
+        {
+            Instantiate(lavaParticles, transform.position - offset, Quaternion.identity);
+        }
+    }
+}
